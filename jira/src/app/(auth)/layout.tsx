dc3b1bf391caf3086726a9412c5ac9838a57d90e
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-10-17 17:20:12 +0900

    ㅇ
    ㅇ

--- jira/src/app/(auth)/layout.tsx
+++ jira/src/app/(auth)/layout.tsx
@@ -1,35 +1,36 @@
 "use client";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-interface AuthLayoutProps {
-    children: React.ReactNode;
-};
-const Authlayout = ({children}: AuthLayoutProps) => {
-    const pathname = usePathname();
-    const isSignIn = pathname === "/sign-in";
 
-    return ( 
-        <main className="bg-neutral-100 min-h-screen">
-            <div className="mx-auto max-w-screen-2xl p-4">
-                <nav className="flex justify-between items-center">
-                    <div className="flex items-center gap-2">
-                        <Image src="/logo.svg" alt="logo" width = {152} height = {56}/>
-                    <div className="flex items-center gap-2">
-                        <Button asChild variant = "secondary">
-                            <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
-                        {isSignIn ? "Sign Up":"Login"}
-                        </Link>
-                        </Button>
-                    </div>
-                    </div>
-                </nav>
-            </div>
-            <Image src="/logo.svg" height={50} width = {100} alt = "Logo"/>
-            {children}
-        </main>
-    );
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
 }
 
-export default Authlayout; 
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          <Button asChild variant="secondary">
+            <Link href={isSignIn ? "sign-up" : "/sign-in"}>
+              {isSignIn ? "Sign Up" : "Login"}
+            </Link>
+          </Button>
+        </nav>
+        <div className="flex flex-col items-center justify-senter pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayout;
--- jira/src/components/dotted-separator.tsx
+++ jira/src/components/dotted-separator.tsx
@@ -7,37 +7,40 @@ interface DottedSeparatorProps {
   dotSize?: string;
   gapSize?: string;
   direction?: "horizontal" | "vertical";
-};
+}
 
-export const DottedSeparator /*DottedSeparatorProps*/ = ({
-    className,
-    color = "d4d4d8",
-    height = "2px",
-    dotSize = "2px",
-    direction = "horizontal"
+export const DottedSeparator = ({
+  className,
+  color = "#d4d4dB",
+  height = "2px",
+  dotSize = "2px",
+  gapSize = "6px",
+  direction = "horizontal",
 }: DottedSeparatorProps) => {
-    const isHorizontal = direction === "horizontal";
+  const isHorizontal = direction === "horizontal";
 
-    return (
-        <div className={cn(
-        isHorizontal ? "w-full flex items-center": "h-full flex flex-col items-center"
-        className,
-    )}>
+  return (
+    <div
+      className={cn(
+        isHorizontal
+        ? "w-full flex items-center"
+        : "h-full flex flex-col items-center",
+        className
+    )}
+    >
     <div
         className={isHorizontal ? "flex-grow" : "flex-grow-0"}
         style={{
-            width: isHorizontal ? "100%" : height,
-            height: isHorizontal? height : "100%",
-            backgroundImage: `radial-gradient(circle, ${color} 25%, transparent 25%)`,
-            ? `${paresInt(dotSize) + parseInt(gapSize)}px ${height}`
-            : `${height} ${parseInt(dotSize)parseInt(gapSize)}px`,
-            backgroundRepeat: isHorizontal ? "repeat-x" : "repeat-y"
-            backgroundPosition: "center",
+        width: isHorizontal ? "100%" : height,
+        height: isHorizontal ? height : "100%",
+        backgroundImage: `radial-gradient(circle, ${color} 25%, transparent 25%)`,
+        backgroundSize: isHorizontal
+        ? `${parseInt(dotSize) + parseInt(gapSize)}px ${height}`
+        : `${height} ${parseInt(dotSize) + parseInt(gapSize)}px`,
+        backgroundRepeat: isHorizontal ? "repeat-x" : "repeat-y",
+        backgroundPosition: "center",
         }}
     />
-
-        </div>
-    )
-
-    
+    </div>
+  );
 };
