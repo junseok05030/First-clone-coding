30db55267ddec3d0ba7f0d653ff9ce68be32995e
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-10-29 19:36:44 +0900

    혼란을 틈탄 야무진 커미커미커밋

--- jira/src/features/auth/components/user-button.tsx
+++ jira/src/features/auth/components/user-button.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Loader } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
     DropdownMenu,
@@ -13,8 +13,31 @@ import { useLogout } from "../api/use-logout";
 import { useCurrent} from "../api/use-current";
 
 export const UserButton = () => {
+    const { data: user, isLoading } = useCurrent();
+    
+    if (isLoading) {
     return (
+        <div className="size-10 rounded-full flex items-center justify-center bg-neutral-200 border border-neutral-300">
+            <Loader className="size-4 animate-spin text-muted-foreground"/>
+        </div>
+        );
+    }
 
-    );
+    if(!user) {
+        return null;
+    }
+
+    const { name, email } = user;
+
+    const AvatarFallback = name
+    ? name.charAt(0).toUpperCase()
+    : email.charAt(0).toupperCase() ?? "U";
+    return (
+        <Avatar>
+            <AvatarFallback>
+
+            </AvatarFallback>
+        </Avatar>
+    )
 };
 
