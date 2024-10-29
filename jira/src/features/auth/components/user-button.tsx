ba08422a567568be59bb9c59c8dc3f7a9b18b7ea
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-10-29 19:27:11 +0900

    아 심심해

---
+++ jira/src/features/auth/components/user-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DottedSeparator } from "@/components/dotted-separator";
+
+import { useLogout } from "../api/use-logout";
+import { useCurrent} from "../api/use-current";
+
+export const UserButton = () => {
+    return (
+
+    );
+};
+
