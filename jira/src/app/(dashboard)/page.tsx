8cb8fa3020b5c60f706791d454de949d9eef22ee
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-10-31 18:04:53 +0900

    ㄹㄹㄹㄹㄹㄹㄹㄹ

--- jira/src/app/(dashboard)/page.tsx
+++ jira/src/app/(dashboard)/page.tsx
@@ -1,15 +1,10 @@
 import { getCurrent } from "@/features/auth/actions";
-
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-    const user = await getCurrent();
+  const user = await getCurrent();
 
-    if (!user) redirect("/sign-in");
+  if (!user) redirect("/sign-in");
 
-    return (
-        <div>
-            This is a home page
-        </div>
-    );
-};
+  return <div>This is a home page</div>;
+}
--- jira/src/components/moblie-sidebar.tsx
+++ jira/src/components/moblie-sidebar.tsx
@@ -9,23 +9,23 @@ import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 export const MoblieSidebar = () => {
-    const [isOpen, setIsopen] = useState(false);
-    const pathname = usePathname();
+  const [isOpen, setIsopen] = useState(false);
+  const pathname = usePathname();
 
-    useEffect(() => {
-        setIsopen(false);
-    }, [pathname]);
+  useEffect(() => {
+    setIsopen(false);
+  }, [pathname]);
 
-    return (
+  return (
     <Sheet modal={false} open={isOpen} onOpenChange={setIsopen}>
-        <SheetTrigger asChild>
+      <SheetTrigger asChild>
         <Button variant="secondary" className="lg:hidden">
-            <MenuIcon className="size-4 text-neutral-500" />
+          <MenuIcon className="size-4 text-neutral-500" />
         </Button>
-    </SheetTrigger>
-    <SheetContent side="left" className="p-0">
-            <Sidebar />
-        </SheetContent>
-        </Sheet>
-    );
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
 };
