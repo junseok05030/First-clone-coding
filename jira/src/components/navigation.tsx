569e87e5a07116c31caf00e42332b6117c594579
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-10-31 18:08:21 +0900

    ㅋㅁ

--- jira/src/components/navigation.tsx
+++ jira/src/components/navigation.tsx
@@ -1,8 +1,12 @@
+import { cn } from "@/lib/utils";
 import { SettingsIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
-import { GoCheckCircle, GoCheckCircleFill, GoHome, GoHomeFill } from "react-icons/go";
-
-import { cn } from "@/lib/utils";
+import {
+  GoCheckCircle,
+  GoCheckCircleFill,
+  GoHome,
+  GoHomeFill,
+} from "react-icons/go";
 
 const routes = [
   {
@@ -32,23 +36,25 @@ const routes = [
 ];
 
 export const Navigation = () => {
-    return (
-        <ul className="flex flex-col">
-            {routes.map((item) => {
-                const isActive = false;
-                const Icon = isActive ? item.activeIcon : item.icon;
-                return(
-                    <Link key ={item.href} href={item.href}>
-                        <div className={cn(
-                            "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
-                            isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
-                        )}>
-                            <Icon className="size-5 text-neutral-500" />
-                            {item.label}
-                        </div>
-                    </Link>
-                )
-            })}
-        </ul>
-    )
-}
+  return (
+    <ul className="flex flex-col">
+      {routes.map((item) => {
+        const isActive = false;
+        const Icon = isActive ? item.activeIcon : item.icon;
+        return (
+          <Link key={item.href} href={item.href}>
+            <div
+              className={cn(
+                "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
+                isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+              )}
+            >
+              <Icon className="size-5 text-neutral-500" />
+              {item.label}
+            </div>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+};
